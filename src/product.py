8d90c5deb259850e1ae81050501675ef3d605a45
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-02-18 19:07:42 +0300

    product_is_ready

---
+++ src/product.py
@@ -0,0 +1,30 @@
+class Product:
+    """Класс товары"""
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+class Category:
+    """Класс категории товаров"""
+    name: str
+    description: str
+    products: str
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.category_count += 1
+        Category.product_count += len(self.products)
+
+
