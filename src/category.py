110223b9e38adefb1b7bf2a424abc3879c71be0d
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-02-21 22:10:04 +0300

    category_is_ready

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Класс категории товаров"""
 
@@ -11,14 +14,31 @@ class Category:
         """Инициализация экземпляра класса 'Category'"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
+
+    def add_product(self, product: Product) -> None:
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self) -> str:
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
 
 
 if __name__ == "__main__":
     category1 = Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        ["product1", "product2", "product3"],
+        [],
     )
+    product4 = Product("S G S23 Ultra", "Серый", 180000.0, 5)
+    category1.add_product(product4)
+    product5 = Product("Ultra", "Серый", 180000.0, 5)
+    category1.add_product(product5)
+    print(category1.products)
