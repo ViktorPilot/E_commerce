a9b1f74adfc632107d45739adaff7b260d20a21f
Viktor Bukotin <viktorbukotin0@gmail.com>
2026-02-28 19:42:38 +0300

    task_16_1_to_revise

--- tests/test_prod_grass.py
+++ tests/test_prod_grass.py
@@ -2,7 +2,7 @@ from src.prod_grass import LawnGrass
 
 
 def test_init_grass(grass_1: LawnGrass) -> None:
-    """Тестирование инициализации товара категории 'LawnGrass'"""
+    """Тестирование инициализации товара категории 'LawnGrass' при валидных значениях"""
     assert grass_1.name == "Газонная трава"
     assert grass_1.price == 500.0
     assert grass_1.country == "Россия"
--- tests/test_prod_smartphone.py
+++ tests/test_prod_smartphone.py
@@ -2,7 +2,7 @@ from src.prod_smartphone import Smartphone
 
 
 def test_init_grass(smartphone_1: Smartphone) -> None:
-    """Тестирование инициализации товара категории 'Smartphone'"""
+    """Тестирование инициализации товара категории 'Smartphone' при валидных значениях"""
     assert smartphone_1.name == "Samsung Galaxy S23 Ultra"
     assert smartphone_1.price == 180000.0
     assert smartphone_1.efficiency == 95.5
